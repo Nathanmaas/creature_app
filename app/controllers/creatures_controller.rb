f440056ecc45b83f7ef997b6ac3f3730d99e4516
Nathan Maas <nathansmaas@gmail.com>
2015-07-20 22:00:27 -0700

    updated

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,31 +3,39 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+  def show
+    @creatures = Creature.find(params[:id])
+  end
 
-#   def show
-#     @creatures = Creature.find(params[:id])
-#   end
+  def new
+    @creature = Creature.new
+  end
 
-#   def new
-#     @creature = Creature.new
-#   end
+  def create
+    Creature.create creature_params
+    redirect_to creatures_path
+  end
 
-#   def create
-#     Creature.create creature_params
-#     redirect_to creatures_path
-#   end
+  def edit
+    @creatures = Creature.find(params[:id])
+  end
 
-#   def edit
-#     @creatures = Creature.find(params[:id])
-#   end
+  def update
+    c = Creature.find params[:id]
+    c.update creature_params
+    redirect_to creatures_path
+  end
+  def destroy
+    Creature.find(params[:id]).delete
+    redirect_to creatures_path
+  end
 
-#   def update
-#     c = Creature.find params[:id]
-#     c.update creature_params
-#     redirect_to creatures_path
-#   end
+private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 
-# end
+end
 
 
 
